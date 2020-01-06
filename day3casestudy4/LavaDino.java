e8e103aca9a72fa3504f9591f89b7b9a7eadf31d
EsakkiammalEsak <49179825+EsakkiammalEsak@users.noreply.github.com>
2020-01-06 12:48:16 +0530

    inheritance
    inheritance case study program 4th

---
+++ day3casestudy4/LavaDino.java
@@ -0,0 +1,42 @@
+package day3casestudy3;
+
+public class LavaDino extends Dinosaur {
+	protected String breath;
+	protected String heat;
+
+	public LavaDino(String species, String consumptiontype, String breath, String heat) {
+	super(species, consumptiontype);
+	this.breath = breath;
+	this.heat = heat;
+	}
+
+
+	public String getBreath() {
+	return breath;
+	}
+
+
+	public void setBreath(String breath) {
+	this.breath = breath;
+	}
+
+
+	public String getHeat() {
+	return heat;
+	}
+	
+	public void setHeat(String heat) {
+		this.heat = heat;
+		}
+
+
+		public void displaydinodetails()
+		{
+			System.out.println("Lava Report");
+			System.out.println("\n");
+			System.out.println("Lava Dino Properties : ");
+		System.out.println(species+" is a "+consumptiontype);
+		 
+		}
+
+}
